56d894adaafd5c5b656be6025fe2ae662be9f401
sdfiiiiii <13222737875@163.com>
2013-12-13 10:25:08 +0800

    springmvc

---
+++ src/com/tanfei/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.tanfei.controller;
+ 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+ 
+/**
+ * µÇÂ¼¿ØÖÆÆ÷
+ * @author tanfei
+ * @date Feb 1, 2013 9:38:13 AM
+ */
+@Controller
+public class LoginController {
+ 
+    @RequestMapping("/login")
+    public String toLogin()
+    {
+    	System.out.println("helloooo");
+       return "login";
+    }
+}
